0df05e4b74db0182b78c1d157f8385937b89b290
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-05-09 17:33:18 +0200

    feat: connect router with controller

--- routers/moviesRouter.js
+++ routers/moviesRouter.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+//movies controller
+const controller = require('../controllers/moviesController');
+
 //index
-router.get('/', index);
+router.get('/', controller.index);
 
 //show
-router.get('/:id', show);
+router.get('/:id', controller.show);
+
+module.exports = router;
