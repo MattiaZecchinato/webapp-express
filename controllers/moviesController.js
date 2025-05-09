34ffe72d32a80bc73fe9af2ec71ff5cef6e43f13
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-05-09 18:14:34 +0200

    feat: add query for movies show path

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -25,11 +25,51 @@ function index(req, res) {
 //show
 function show(req, res) {
 
-    const { id } = req.params;
+    const id = parseInt(req.params.id);
 
-    const message = `show path with id: ${id}`;
+    const moviesSql = 'SELECT * FROM movies WHERE id = ?';
 
-    res.send(message);
+    const reviewsSql = `SELECT reviews.* FROM reviews 
+                            JOIN movies ON movies.id = reviews.movie_id 
+                            WHERE movies.id = ?`
+
+    connection.query(moviesSql, [id], (err, moviesResult) => {
+
+        if(err) {
+
+            return res.status(500).json({
+                status: '500',
+                error: 'Query error'
+            });
+        }
+
+        if(moviesResult.length === 0) {
+
+            return res.status(404).json({
+                status: '404',
+                error: 'Movies not found'
+            });
+        }
+
+        let currentMovie = moviesResult[0];
+
+        connection.query(reviewsSql, [id], (err, reviewsResult) => {
+
+            if(err) {
+
+                return res.status(500).json({
+                    status: '500',
+                    error: 'Query error'
+                });
+            }
+
+            currentMovie.reviews = reviewsResult;
+
+            res.json(currentMovie);
+        });
+    });
+
+    console.log(`show path with id: ${id}`);
 }
 
 module.exports = { index, show };
